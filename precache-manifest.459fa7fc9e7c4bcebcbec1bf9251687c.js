4542f2c4467b920493a37604142748cd0663ea24
Code Sandbox <hello@codesandbox.io>
2023-11-22 09:31:43 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.459fa7fc9e7c4bcebcbec1bf9251687c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98f563176d56123104827bd5df31f141",
+    "url": "https://ridwan-lawal.github.io/csb-nk2zgr/index.html"
+  },
+  {
+    "revision": "21b29251a98091648bcd",
+    "url": "https://ridwan-lawal.github.io/csb-nk2zgr/static/css/main.11f025ee.chunk.css"
+  },
+  {
+    "revision": "bb70b34ce8a5a14b5090",
+    "url": "https://ridwan-lawal.github.io/csb-nk2zgr/static/js/2.dc43aeb0.chunk.js"
+  },
+  {
+    "revision": "21b29251a98091648bcd",
+    "url": "https://ridwan-lawal.github.io/csb-nk2zgr/static/js/main.d7b8c6e5.chunk.js"
+  },
+  {
+    "revision": "605a660eb8cb52c4bd82",
+    "url": "https://ridwan-lawal.github.io/csb-nk2zgr/static/js/runtime~main.875f9d35.js"
+  }
+]);
